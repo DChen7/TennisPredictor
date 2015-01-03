1c502c3fa91b5349b7cda3ea045e802e43a763f9
Daniel Chen <chen.daniel7@gmail.com>
2015-01-03 17:04:10 -0500

    cleanup

--- tennis_classifier.py
+++ tennis_classifier.py
@@ -48,20 +48,8 @@ def teardown_request(exception):
 	if db is not None:
 		db.close()
 
-# def make_dicts(cursor, row):
-# 	return dict((cur.description[idx][0], value)
-# 	for idx, value in enumerate(row))
-
-# def query_db(query, args=(), one=False):
-# 	cur = connect_db().cursor().execute(query, args)
-# 	rv = cur.fetchall()
-# 	cur.close()
-# 	return (rv[0] if rv else None) if one else rv
-
 @app.route('/')
 def show_entries():
-	# cur = g.db.execute('select * from players order by name desc')
-	# entries = [dict(name=row[0], height=row[1], weight=row[2], left_handed=row[3], age=row[4]) for row in cur.fetchall()]
 	return render_template('show_entries.html')
 
 @app.route('/predict', methods=['POST'])
@@ -77,7 +65,6 @@ def prediction():
 		features = extract_features(player1, player2)
 		classifier = joblib.load("/Users/danielchen/Documents/Flask Projects/TennisClassifier/static/tennis_classifier.pkl")
 		prediction = classifier.predict(features)
-		print prediction
 		if prediction[0] == 1:
 			winner = player1
 		else:
@@ -91,15 +78,11 @@ def extract_features(player1, player2):
 	hand = 0
 	player1_query = 'SELECT * from players where name = \"%s\"' % player1
 	player2_query = 'SELECT * from players where name = \"%s\"' % player2
-	print player1_query
-	print player2_query
 
 	db = connect_db()
 	cur = db.cursor()
 	player1_features = cur.execute(player1_query)
 	for row in player1_features:
-		print "player 1"
-		print row
 		age_diff += row[3]
 		height_diff += row[4]
 		weight_diff += row[2]
@@ -109,8 +92,6 @@ def extract_features(player1, player2):
 	cur = db.cursor()
 	player2_features = cur.execute(player2_query)	
 	for row in player2_features:
-		print "player 2"
-		print row
 		age_diff -= row[3]
 		height_diff -= row[4]
 		weight_diff -= row[2]
@@ -122,7 +103,6 @@ def extract_features(player1, player2):
 			hand += 3
 		else:
 			hand = 0
-	print [age_diff, height_diff, weight_diff, hand]
 	return [age_diff, height_diff, weight_diff, hand]
 
 if __name__ == '__main__':
